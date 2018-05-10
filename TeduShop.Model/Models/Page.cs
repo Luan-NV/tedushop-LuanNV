ae4c549257fd007f11abe919db6af354e1f96294
LuanNV <luanvne@outlook.com>
2018-05-10 10:02:22 +0700

    add domain entities for project

---
+++ TeduShop.Model/Models/Page.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Pages")]
+    public class Page : AudiTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(256)]
+        [Required]
+        public string Alias { set; get; }
+
+        public string Content
+        {
+            set; get;
+        }
+    }
+}
